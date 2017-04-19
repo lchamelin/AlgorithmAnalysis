ca6ea0759afc906844287bf0430f000465729b92
raph63 <raphael.christian-roy@polymtl.ca>
2017-04-19 17:27:51 -0400

    Add tp.sh mais rien connecte encore = je fait en revenant de levent

---
+++ tp3-algo-raph/tp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+OPTIONS=""
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -a|--algo)
+    ALGO="$2"
+    shift
+    ;;
+    -e|--ex_path)
+    EX_PATH="$2"
+    shift
+    ;;
+    -p|--print|-t|--time)
+    OPTIONS="${OPTIONS}${1} "
+    ;;
+    *)
+        echo "Argument inconnu: ${1}"
+        exit
+    ;;
+esac
+shift
+done
+
+java ./tp3.java $ALGO $EX_PATH $OPTIONS
